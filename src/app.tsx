3afae810122114374efad2ea0acfb7e83ba42283
Théo SCHMIDT <rocknologie@hotmail.fr>
2019-10-24 12:17:24 +0200

    Commit App.css --> app.css

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
+import './app.css';
 
 export const App: React.FC = () => (
     <BrowserRouter>
