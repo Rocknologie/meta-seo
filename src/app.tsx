e33d01d0feaf666bd168e2170c8896ab796248b7
Théo SCHMIDT <rocknologie@hotmail.fr>
2019-10-31 11:06:13 +0100

    Commit change Navbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react'
 import './app.css';
 import { MainPage } from './main-page';
+import { AgencePage } from './agence-page';
+import { Navbar } from './navbar';
 
 export const App: React.FC = () => (
     <BrowserRouter>
+        <Container>
+            <Navbar></Navbar>   
+        </Container>
         <Switch>
             <Route exact path="/" component={MainPage} />
-            <Route />
+            <Route exact path="/agence" component={AgencePage}/>
         </Switch>
     </BrowserRouter>
 )
