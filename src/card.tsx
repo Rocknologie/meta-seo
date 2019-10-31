9bb2edc3a5e8c69b43725294f12dac44a9b98293
Théo SCHMIDT <rocknologie@hotmail.fr>
2019-10-31 11:40:46 +0100

    Commit card pour R

--- src/card.tsx
+++ src/card.tsx
@@ -11,8 +11,12 @@ export const ServiceCard: React.FC<ServiceCardProps> = (props) => (
     <Card color='yellow'>
         <Image src={props.img} size='medium' wrapped ui={false}/>
         <Card.Content color='blue'>
-            <Card.Header ><p>{props.title}</p></Card.Header>
-            <Card.Description><p>{props.content}</p></Card.Description>           
+            <Card.Header >
+                <p>{props.title}</p>
+            </Card.Header>
+            <Card.Description>
+                <p>{props.content}</p>
+            </Card.Description>           
         </Card.Content>
         <Card.Content extra>
         <Button color='blue'>
