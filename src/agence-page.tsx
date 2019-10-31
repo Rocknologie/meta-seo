11ae282ddc0c0fca7b0dc4538fd55cdd7df585ad
Théo SCHMIDT <rocknologie@hotmail.fr>
2019-10-31 11:34:44 +0100

    Commit page agence

---
+++ src/agence-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { } from 'react-router-dom';
+import './app.css';
+
+export const AgencePage: React.FC = () => (
+    <div>
+        <p>ICI C'EST PARIS !</p>
+    </div>
+)
