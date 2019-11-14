87230ff3b02d85186e861972a136c7fbe18e6da7
Théo SCHMIDT <rocknologie@hotmail.fr>
2019-11-14 11:54:35 +0100

    Changes

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,14 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { Container } from 'semantic-ui-react'
 import './app.css';
 import { MainPage } from './main-page';
-import { AgencePage } from './agence-page';
+import { AgencePage } from './agence/agence-page';
 import { Header } from './header';
 import { Footer } from './footer';
-import { Contact } from './contact';
+import { ContactPage } from './components/contact';
+import { AgenceLyonPage } from './agence/agence-lyon';
+import { AgenceParisPage } from './agence/agence-paris';
+import { AgenceLillePage } from './agence/agence-lille';
+import { AgenceBordeauxPage } from './agence/agence-bordeaux';
 
 export const App: React.FC = () => (
     <BrowserRouter>
@@ -23,9 +27,21 @@ export const App: React.FC = () => (
                 </Route>
                 <Route exact path="/agence">
                     <AgencePage />
-                </Route>            
+                </Route>    
+                <Route exact path='/agence-lyon'>
+                    <AgenceLyonPage />
+                </Route>
+                <Route exact path='/agence-paris'>
+                    <AgenceParisPage />
+                </Route>
+                <Route exact path='/agence-lille'>
+                    <AgenceLillePage />
+                </Route>
+                <Route exact path='/agence-bordeaux'>
+                    <AgenceBordeauxPage />
+                </Route>    
                 <Route exact path="/contact">
-                    <Contact />
+                    <ContactPage />
                 </Route>
                 <Route path='/'>
                     <Footer />
--- src/list-country.tsx
+++ src/list-country.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { Grid, Image, Popup } from 'semantic-ui-react'
-import { PopupCity } from './popup-city'
+import { PopupCity } from './components/popup-city'
 
 export const ListCountry: React.FC = () => (
-    <div>
-        <p>Vous pouvez retrouver les conseillers de notre agence SEO en direct dans nos locaux à Paris, Lyon, Bordeaux et Lille. Évidemment, nous sommes également disponible en ligne par téléphone ou par visioconférence. Tout autre moyen de communication est possible, nous nous adaptons à vos envies.</p>
+    <div>        
         <Grid>
+            <Grid.Row columns={1} textAlign='center'>
+                <p>Vous pouvez retrouver les conseillers de notre agence SEO en direct dans nos locaux à Paris, Lyon, Bordeaux et Lille. Évidemment, nous sommes également disponible en ligne par téléphone ou par visioconférence. Tout autre moyen de communication est possible, nous nous adaptons à vos envies.</p>
+            </Grid.Row>
+
+            <Grid.Row columns={1}>
+                <PopupCity alt='Ville de Lyon' src='/img/city/lyon.jpg' content='Lyon' href='/agence-lyon'/>
+            </Grid.Row>
+            
+            <Grid.Row columns={1}>
+                <PopupCity alt='Ville de Paris' src='/img/city/paris.jpg' content='Paris' href='/agence-paris'/>   
+            </Grid.Row>
+            <Grid.Row columns={1}>
+                <PopupCity alt='Ville de Lille' src='/img/city/lille.jpg' content='Lille' href='/agence-lille'/>   
+            </Grid.Row>
+            <Grid.Row columns={1}>
+                <PopupCity alt='Ville de Bordeaux' src='/img/city/bordeaux.jpg' content='Bordeaux' href='/agence-bordeaux'/> 
+            </Grid.Row>
+        </Grid>
+                
+    </div>
+)
+
+/*
+<Grid>
             <Grid.Row columns={4}>
                 <Grid.Column width={4}>
                     <PopupCity alt='Ville de Lyon' src='/img/city/lyon.jpg' content='Lyon' href='https://www.google.fr/maps/place/Lyon/@45.75801,4.8001016,13z/data=!3m1!4b1!4m5!3m4!1s0x47f4ea516ae88797:0x408ab2ae4bb21f0!8m2!3d45.764043!4d4.835659'/>
-                    </Grid.Column>
+                </Grid.Column>
                 <Grid.Column width={4}>
                     <PopupCity alt='Ville de Paris' src='/img/city/paris.jpg' content='Paris' href='https://www.google.fr/maps/place/Paris/@48.8589507,2.2770205,12z/data=!3m1!4b1!4m5!3m4!1s0x47e66e1f06e2b70f:0x40b82c3688c9460!8m2!3d48.856614!4d2.3522219'/>
                 </Grid.Column>
@@ -21,5 +44,4 @@ export const ListCountry: React.FC = () => (
                 </Grid.Column>   
             </Grid.Row>  
         </Grid>
-    </div>
-)
+*/
