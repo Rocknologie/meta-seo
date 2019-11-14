881b3276ac58d41d9c18d996f66534d5592af129
Lelberto <jsurieux@gmail.com>
2019-11-14 21:06:55 +0100

    Fix title

--- src/main-page.tsx
+++ src/main-page.tsx
@@ -14,7 +14,7 @@ export const MainPage: React.FC = () => (
         
           <LineFeed />    
                    
-          <Header as='h1' textAlign='left'>Agence de référencement de site web</Header>
+          <Header as='h1' textAlign='left'>Meta-SEO - L'agence de référencement de site web</Header>
 
           <p>Le référencement de site web est devenu un point clé pour générer du trafic sur votre site web. Une bonne visibilité ainsi qu’une performance accrue sont primordiales pour parvenir à répondre aux besoins de vos clients ou de vos internautes, tout type de site confondu (e-commerce, forum, services, etc…).</p>
 
