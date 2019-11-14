0ca6a6ee442c89fb25ea9535195a7de83a069ce7
Théo SCHMIDT <rocknologie@hotmail.fr>
2019-11-14 23:01:15 +0100

    fin du projet

--- src/articles/article-3.tsx
+++ src/articles/article-3.tsx
@@ -13,7 +13,7 @@ export const Article3Page: React.FC = () => (
         <LineFeed />    
 
         <Header as='h1' color='black'>Pourquoi avoir recours à une agence de seo ?</Header> 
-        <Image alt='agence de seo' src='' />        
+        <Image alt='agence de seo' src='/img/articles/agence-de-seo.png' size='large'/>        
         
         <p>Le référencement (également appelé SEO pour Search Engine Optimization), c’est un domaine vaste et complexe, mais sans lui votre site web pourrait ne pas être aussi visible que vous le souhaitez. Avoir recours à une agence de SEO plutôt que de devoir tout apprendre sur le tas pourrait vous faire gagner beaucoup de temps et d’argent, mais c’est également une question d’expertise car il ne faut pas faire n’importe quoi : le référencement est peuplé de bonnes pratiques qui, si elles ne sont pas respectées, pourraient inverser la donne et réduire la visibilité de votre site web.</p>
         
@@ -25,7 +25,7 @@ export const Article3Page: React.FC = () => (
         {/* Paragraphe 2 */}
         <Header as='h2' color='black'>Un large choix de services</Header> 
         <p>Une agence de SEO peut proposer plusieurs services de référencement différents, et il faut savoir à lequel nous devons faire affaire pour ne pas partir dans quelque chose d’inutile sur le long terme. Que choisir si l’on veut augmenter sa position dans les résultats de recherches de Google ? Et si je veux simplement améliorer mes réseaux sociaux ?</p>
-        <Image alt='question' src='' />
+        <Image alt='question' src='/img/articles/question.png' size='large'/>
         <p>Pour le savoir, nous allons vous exposer une liste non exhaustive de services parmis les plus connus et ce qu’ils en sont concrètement.</p>
 
 
@@ -35,7 +35,7 @@ export const Article3Page: React.FC = () => (
         <p>Le petit frère du référencement naturel, ou plutôt devrions-nous dire son grand frère depuis peu, car aujourd’hui, c’est sur les smartphones que l’on fait le plus de recherches sur Internet. Le référencement mobile, c’est le résultat d’une optimisation de votre site web ou de votre application mobile.</p>
         <Header as='h3' color='black'>Le référencement social</Header>
         <p>Également appelé SMO (Social Media Optimization), ce type de référencement se fait au niveau des réseaux sociaux. En effet, la visibilité sur ceux-ci peut contribuer à un flux de trafic largement amélioré si vos comptes sont optimisés. Contenu des posts, images, description, tout est revu à la loupe.</p>
-        <Image alt='types de référencement' src='' />
+        <Image alt='types de référencement' src='/img/articles/types-de-referencement.png' size='large'/>
         <p>Si vous ne savez toujours pas où partir, certaines agences de SEO sont également équipées d’outils puissants d’audit SEO afin de laisser les experts vous diriger vers la solution qui répond parfaitement à vos attentes.</p>
 
     </div>
--- src/services/netlinking.tsx
+++ src/services/netlinking.tsx
@@ -1,6 +1,36 @@
 import React from 'react'
-import { Form, Button, Header, Icon } from 'semantic-ui-react'
+import { AsideComponent } from '../components/aside'
+import { Grid, Header, Image } from 'semantic-ui-react'
+import { MetaTags } from 'react-meta-tags'
+import { LineFeed } from '../components/line-feed'
 
 export const NetlinkingPage: React.FC = () => (
-    <div></div>
+    <div>
+        <MetaTags>
+            <title>SEO Backlinks - Meta-SEO</title>
+            <meta name="description" content=' La stratégie des SEO backlinks a pour but d’augmenter la popularité de votre site web. Le but est de vous faire connaître auprès des sites web ayant le même thème que le votre et de gagner la confiance de vos internautes.' />
+        </MetaTags>
+        
+        <LineFeed />
+        <Grid>
+            <Grid.Column only='widescreen' widescreen={12}>
+                <Header as='h1' textAlign='left'>Le Netlinking : SEO backlinks</Header>
+                <p>Le netlinking, également appelé linking externe ou SEO backlinks, est un procédé qui permet d’augmenter le nombre de liens vers un site. Cette stratégie a pour but d’augmenter la popularité de votre site web, et par conséquent, générer toujours plus de trafic vers celui-ci. Le but est de vous faire connaître auprès des sites web ayant le même thème que le votre, mais c’est aussi un moyen de gagner la confiance de vos internautes si ces liens sont pertinents et intéressants pour eux.</p>
+                <Image src='/img/' size='large' alt='Backlink'/>
+                <p>Les algorithmes des moteurs de recherche traitent et comptent les liens pointant vers votre site, pour ensuite créer une sorte d’indice de confiance qui va entrer en jeu dans le positionnement de votre site web sur les pages de résultats de ceux-ci lorsque les internautes saisissent les bons mots-clés. Nous pouvons également faire le travail que font ces algorithmes afin de mieux repérer les sources contenant des liens vers notre site web. Cela permet, en plus d’analyser la popularité et la confiance de ces sources, de visualiser ou est-ce que l’on parle de nous et dans quels domaines.</p>
+
+                {/* Paragraphe 1 */}
+                <Header as='h2' textAlign='left'>Les liens et leur utilité</Header>
+                <p>Les liens entre les pages web constituent un élément fondamental du référencement. Google parcours les pages grâce à ceux-ci, et c’est pour cela qu’il faut concevoir un bon maillage interne au sein de votre site web. Cependant attention à ne pas en faire trop : ce qu’on appelle les “fermes à liens” sont interdites et sanctionnées par Google.
+                    Il faut également produire un contenu riche et intéressant afin d’avoir des liens pertinents pour vos internautes.
+                </p>
+                <Image src='/img/' size='large' alt='Algorithme et backlinks'/>
+                <p>Notre prestation vous permettra de gagner beaucoup de visibilité grâce à nos experts qui mettront tout en oeuvre pour générer des SEO backlinks, mais également crawler régulièrement votre site internet afin d’analyser continuellement les liens externes et leurs sources.</p>
+            </Grid.Column> 
+
+            <Grid.Column only='widescreen' widescreen={4}>
+                <AsideComponent />
+            </Grid.Column>  
+        </Grid>        
+    </div>
 )
