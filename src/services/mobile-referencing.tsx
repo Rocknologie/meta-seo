858cd87c5a19f0e112152011c2856374c8238033
Théo SCHMIDT <rocknologie@hotmail.fr>
2019-11-14 12:16:28 +0100

    Add all services pages + blog page

---
+++ src/services/mobile-referencing.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Form, Button, Header, Icon } from 'semantic-ui-react'
+
+export const MobileReferncingPage: React.FC = () => (
+    <div></div>
+)
---
+++ src/services/netlinking.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Form, Button, Header, Icon } from 'semantic-ui-react'
+
+export const NetlinkingPage: React.FC = () => (
+    <div></div>
+)
